8317af83f1026b25be036c4a3f8af95421fb8736
Melanie <melanie.collins.p@gmail.com>
2024-04-25 00:29:58 -0400

    update de la lista

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,11 @@
 const formAddTodo = document.querySelector("#form-add-todo")
 const inputTitle = document.querySelector("#input-title")
 const todoList = document.querySelector("#todo-list")
+const formUpdateTodo = document.querySelector("#form-update-todo")
+const inputUpdateTodo = document.querySelector("#input-update-todo")
+const inputUpdateCompleted = document.querySelector("#input-update-completed")
+let idUpdate;
+
 
 formAddTodo.addEventListener('submit', async (event) => {
     event.preventDefault()
@@ -24,6 +29,7 @@ const getTodos = async () => {
         ${item.id} - ${item.title} - ${item.completed}
         <button onclick="deleteTodo('${item.id}')">Eliminar</button>
         </li>
+        <button onclick="formUpdate('${item.id}', '${item.title}', '${item.completed}')">Actualizar</button>
         `
     })
 }
@@ -36,5 +42,21 @@ const deleteTodo = async (id) => {
     getTodos()
 }
 
+const formUpdate = (id, title, completed) => {
+    // console.log(id, title, completed)
+    inputUpdateTodo.value = title
+    inputUpdateCompleted.value = completed
+    idUpdate = id
+}
+
+formUpdateTodo.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const title = inputUpdateTodo.value
+    const completed = inputUpdateCompleted.value
+    const res = await fetch(`/todos/update/${idUpdate}?title=${title}&completed=${completed}`)
+    const data = await res.json()
+    console.log(data)
+    getTodos()
+})
 
 
