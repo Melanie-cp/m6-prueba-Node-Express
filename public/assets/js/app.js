74bbc720d8f3f1a53d754b8170579b4c7ee3c6f2
Melanie <melanie.collins.p@gmail.com>
2024-04-24 23:43:46 -0400

    listar elementos en el html

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,6 @@
 const formAddTodo = document.querySelector("#form-add-todo")
 const inputTitle = document.querySelector("#input-title")
+const todoList = document.querySelector("#todo-list")
 
 formAddTodo.addEventListener('submit', async (event) => {
     event.preventDefault()
@@ -8,7 +9,22 @@ formAddTodo.addEventListener('submit', async (event) => {
 
     const res = await fetch(`/todos/create?title=${title}`)
     const data = await res.json()
-    console.log(data)
-
+    console.log(data) //mostrar al usuario que se agrego con exito
+    getTodos()
 })
+
+const getTodos = async () => {
+    const res = await fetch('/todos')
+    const data = await res.json()
+
+    todoList.innerHTML = ''
+    data.forEach(item => {
+        todoList.innerHTML += `
+        <li>${item.id} - ${item.title} - ${item.completed}</li>
+        `
+    })
+}
+getTodos()
+
+
 
